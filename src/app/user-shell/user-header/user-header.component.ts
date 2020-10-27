e9d335c7633756219c6cb96163e7bacb129f32c4
mitsui <m.mihye1128@gmail.com>
2020-10-27 23:23:31 +0900

    (AuthService) logout Fixes #10

--- src/app/user-shell/user-header/user-header.component.ts
+++ src/app/user-shell/user-header/user-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-user-header',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-header.component.scss'],
 })
 export class UserHeaderComponent implements OnInit {
-  constructor() {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit(): void {}
 }
