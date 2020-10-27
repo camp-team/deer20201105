9c200e63b697dcca030648c10755e19e45fe0abb
mitsui <m.mihye1128@gmail.com>
2020-10-27 09:02:01 +0900

    (prototype) messageListScreen Fixes #4

--- src/app/main-shell/message-list/message-list/message-list.component.ts
+++ src/app/main-shell/message-list/message-list/message-list.component.ts
@@ -6,6 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-list.component.scss'],
 })
 export class MessageListComponent implements OnInit {
+  users = [
+    {
+      name: 'ユーザー名',
+      avatarURL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      title: 'タイトルが入ります。',
+      message: 'メッセージ本文が入ります。',
+      status: 'leaved',
+      joinedDate: '2019.12',
+      leavedDate: '2020.04',
+    },
+    {
+      name: 'ユーザー名',
+      avatarURL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      title: 'タイトルが入ります。',
+      message: 'メッセージ本文が入ります。',
+      status: 'leaved',
+      joinedDate: '2019.12',
+      leavedDate: '2020.04',
+    },
+    {
+      name: 'ユーザー名',
+      avatarURL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      title: 'タイトルが入ります。',
+      message: 'メッセージ本文が入ります。',
+      status: 'leaved',
+      joinedDate: '2019.12',
+      leavedDate: '2020.04',
+    },
+    {
+      name: 'ユーザー名',
+      avatarURL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      title: 'タイトルが入ります。',
+      message: 'メッセージ本文が入ります。',
+      status: 'playing',
+      joinedDate: '2020.09',
+    },
+    {
+      name: 'ユーザー名',
+      avatarURL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+      title: 'タイトルが入ります。',
+      message: 'メッセージ本文が入ります。',
+      status: 'playing',
+      joinedDate: '2020.09',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
