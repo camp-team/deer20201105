049868290feeb1792dd15858bfe40b6c33c7388f
mitsui <m.mihye1128@gmail.com>
2020-10-28 02:00:27 +0900

    (UserService) createUser Fixes #11

---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,13 @@
+import { firestore } from 'firebase';
+
+export interface User {
+  uid: string;
+  name: string;
+  avatarURL: string;
+  isPublic: boolean;
+  title: string;
+  message: string;
+  status: 'leaved' | 'playing';
+  joinedDate: firestore.Timestamp;
+  leavedDated: firestore.Timestamp;
+}
--- src/app/user-shell/settings/settings.module.ts
+++ src/app/user-shell/settings/settings.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './settings/settings.component';
 
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,7 +13,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SettingsComponent],
@@ -20,6 +21,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SettingsRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    MatSlideToggleModule,
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
