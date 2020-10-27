f728349d37f9647344debbdf7e93d29a3321bc46
mitsui <m.mihye1128@gmail.com>
2020-10-27 09:54:36 +0900

    (prototype) homeScreen Fixes #3

--- src/app/main-shell/home/home.module.ts
+++ src/app/main-shell/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, UserCardComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
